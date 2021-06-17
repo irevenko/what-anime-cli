2802331818d46c930452418ee9bc6f169c61cfe3
Artyom <33123862+NikkoFox@users.noreply.github.com>
2021-06-17 12:07:14 +0300

    Types for the new version of tracemoe api

--- types/types.go
+++ types/types.go
@@ -1,15 +1,30 @@
 package types
 
+// Anilist - types for the anilist info
+type Anilist struct {
+	Id    int `json:"id"`
+	IdMal int `json:"idMal"`
+	Title struct {
+		Native  string `json:"native"`
+		Romaji  string `json:"romaji"`
+		English string `json:"english"`
+	} `json:"title"`
+	Synonyms []string `json:"synonyms"`
+	IsAdult  bool     `json:"isAdult"`
+}
+
 // Response - types for the trace.moe API
 type Response struct {
-	Docs []struct {
-		TitleRomanji string  `json:"title_romaji"`
-		TitleEnglish string  `json:"title_english"`
-		TitleNative  string  `json:"title_native"`
-		Similarity   float64 `json:"similarity"`
-		Episode      int     `json:"episode"`
-		At           float64 `json:"at"`
-		Season       string  `json:"season"`
-		IsAdult      bool    `json:"is_adult"`
-	} `json:"docs"`
+	FrameCount int    `json:"frameCount"`
+	Error      string `json:"error"`
+	Result     []struct {
+		Anilist    Anilist `json:"anilist"`
+		Filename   string  `json:"filename"`
+		Episode    int     `json:"episode"`
+		From       float64 `json:"from"`
+		To         float64 `json:"to"`
+		Similarity float64 `json:"similarity"`
+		Video      string  `json:"video"`
+		Image      string  `json:"image"`
+	} `json:"result"`
 }
